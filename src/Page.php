f8e2c51134d65ea7b065a7d4712f99987abba5b9
Matthew Laver <matthew.laver@reasondigital.com>
2016-08-23 18:21:29 +0100

    added test for description, add local html file for using with test, added getDescription method to class

--- src/Page.php
+++ src/Page.php
@@ -1,15 +1,26 @@
 <?php namespace Laver;
 
+use DOMDocument;
+use DOMXPath;
 use GuzzleHttp\Psr7\Response;
 
 class Page
 {
     /**
      * Page constructor.
+     *
+     * @param Response $response
      */
     public function __construct(Response $response)
     {
         $this->response = $response;
+
+        $domDocument = new DOMDocument();
+        libxml_use_internal_errors(true);
+        $domDocument->loadHTML($response->getBody());
+        libxml_clear_errors();
+
+        $this->domXpath = new DOMXPath($domDocument);
     }
 
     public function getSize()
@@ -19,5 +30,10 @@ class Page
         }
 
         return null;
+    }
+
+    public function getDescription()
+    {
+        return $this->domXpath->query('//meta[@name="description"]/@content')[0]->textContent;
     }
 }
--- tests/PageTest.php
+++ tests/PageTest.php
@@ -7,8 +7,17 @@ class PageTest extends PHPUnit_Framework_TestCase
 {
     public function test_page_has_size()
     {
-        $response = new Response(200, ['Content-Length' => 1066]);
+        $response = new Response(200, ['Content-Length' => 1066], 'Hi');
         $page     = new Page($response);
         $this->assertEquals(1066, $page->getSize());
+    }
+
+    public function test_page_has_description()
+    {
+        $description = "Buy Ripe & ready online from Sainsbury's, the same great quality, freshness and choice you'd find in store. Choose from 1 hour delivery slots and collect Nectar points.";
+        $response = new Response(200, ['Content-Length' => 1066], file_get_contents(__DIR__ . '/page_to_parse.html'));
+        $page = new Page($response);
+        $this->assertEquals($description, $page->getDescription());
+
     }
 }
