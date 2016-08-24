3f52313da0054274ffedd660933d74cf20d7891b
Matthew Laver <matthew.laver@reasondigital.com>
2016-08-24 20:28:57 +0100

    added the option for page factory to create page with products class instead

--- src/PageFactory.php
+++ src/PageFactory.php
@@ -4,11 +4,14 @@ use GuzzleHttp\Client;
 
 class PageFactory
 {
-    public static function create($url)
+    public static function create($url, $withProducts = false)
     {
         $client   = new Client();
         $response = $client->request('GET', $url);
 
+        if ($withProducts) {
+            return new PageWithProducts($response);
+        }
         return new Page($response);
     }
 
