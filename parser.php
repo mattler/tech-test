7bc7ef2e08807a68886c06d2c437b67e07935b78
Matthew Laver <matthew.laver@reasondigital.com>
2016-08-24 20:30:08 +0100

    created initial initiator file

---
+++ parser.php
@@ -0,0 +1,9 @@
+<?php
+
+use Laver\Parser;
+
+require 'vendor/autoload.php';
+
+$url      = 'http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html';
+$response = (new Parser(\Laver\PageFactory::create($url, true)))->run();
+echo json_encode($response, JSON_PRETTY_PRINT) . PHP_EOL;
