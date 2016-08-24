b038ae03c2138e1c1d420583bfb47c3cdcdf94a7
Matthew Laver <matthew.laver@reasondigital.com>
2016-08-24 18:01:34 +0100

    added a page factory

---
+++ src/PageFactory.php
@@ -0,0 +1,15 @@
+<?php namespace Laver;
+
+use GuzzleHttp\Client;
+
+class PageFactory
+{
+    public static function create($url)
+    {
+        $client = new Client();
+        $response = $client->request('GET', $url);
+
+        return new Page($response);
+    }
+
+}
