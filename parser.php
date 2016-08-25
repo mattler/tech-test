b1d59b0abae696487122e44686ac1860f87387e0
Matthew Laver <matthew.laver@reasondigital.com>
2016-08-25 12:49:14 +0100

    added a top level exception catch to show any errors

--- parser.php
+++ parser.php
@@ -5,5 +5,11 @@ use Laver\Parser;
 require 'vendor/autoload.php';
 
 $url      = 'http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html';
-$response = (new Parser(\Laver\PageFactory::create($url, true)))->run();
-echo json_encode($response, JSON_PRETTY_PRINT) . PHP_EOL;
+
+try {
+    $response = (new Parser(\Laver\PageFactory::create($url, true)))->run();
+    echo json_encode($response, JSON_PRETTY_PRINT) . PHP_EOL;
+} catch (Exception $e) {
+    echo "There has been an error." . PHP_EOL;
+    echo $e->getMessage() . PHP_EOL;
+}
